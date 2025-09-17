83a101db42c667e019b75d4fa5727a0982a11dce
Max <maxwelljblanco@gmail.com>
2025-09-17 07:48:33 -0500

    added progressbar component, added to index

--- ProgressBarRNPaper/app/index.tsx
+++ ProgressBarRNPaper/app/index.tsx
@@ -1,5 +1,5 @@
 import { Text, View } from "react-native";
-
+import Bar from "./components/Bar";
 export default function Index() {
   return (
     <View
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Bar></Bar>
     </View>
   );
 }
